4c7f519167fbed325cfca2883d35a9e2f5b3e036
Katorz <katorz.art@gmail.com>
2022-11-23 23:52:44 +0100

    Setup base

---
+++ src/components/Settings.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>Settings</h2>
+  </div>
+</template>
---
+++ src/components/SvgViewer.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <svg viewbox="0 0 100 100" width="100" height="100"></svg>
+  </div>
+</template>
